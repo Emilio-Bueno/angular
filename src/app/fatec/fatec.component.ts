3fd66eae396a85a8c184851bb2f4ddfeceed0c22
Emilio--Bueno <emilio__bueno@outlook.com>
2023-03-31 15:27:32 -0300

    Angular

---
+++ src/app/fatec/fatec.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-fatec',
+  templateUrl: './fatec.component.html',
+  styleUrls: ['./fatec.component.css']
+})
+
+export class FatecComponent {
+
+}
